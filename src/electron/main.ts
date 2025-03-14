222486884aa97d7bd8861b942991093cb9c97377
AdurraIS <giovanniultramari@gmail.com>
2025-03-14 15:48:04 -0300

    Cleaning Project

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,9 +1,14 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu } from 'electron';
 import path from 'path';
 
 const createWindow = () => {
     const mainWindow = new BrowserWindow({});
+
+
     mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+
+    mainWindow.setMenuBarVisibility(false);
 };
 
-app.on('ready', createWindow);
+
+app.whenReady().then(createWindow);
